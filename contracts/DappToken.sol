d538b734ae60db4554ca5edc6a391ecf799dff65
ahmadlatif1084 <ahmad.latif1084@gmail.com>
2019-11-06 21:52:20 +0500

    working on configurations

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract DappToken {
+  uint public totalSupply;
+
+   constructor () public {
+        totalSupply = 1000000;
+    }
+
+    function DappTokenSupply () public {
+        totalSupply = 1000000;
+    }
+}
