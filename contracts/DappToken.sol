b8d93bbbb1352a20ce2fe8e3c971dc5343d568f1
ahmadlatif1084 <ahmad.latif1084@gmail.com>
2019-11-11 00:23:00 +0500

    setup correctly

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,13 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-  uint public totalSupply;
+  uint256 public totalSupply;
 
    constructor () public {
         totalSupply = 1000000;
     }
 
-    function DappTokenSupply () public {
-        totalSupply = 1000000;
-    }
+    // function DappTokenSupply () public {
+    //     totalSupply = 1000000;
+    // }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -6,7 +6,7 @@ return DappToken.deployed().then(function(instance){
 tokenInstance=instance;
 return tokenInstance.totalSupply();
 }).then(function(totalSupply){
-    assert.equal(totalSupply.toNumber,1000000,'sets the total supply to 1,000,000');
+    assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000');
 });
 });
 });
