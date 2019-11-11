983b1b669ed8555b38c216a1af69136d48063679
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-11 19:34:39 +0500

    creating the erc-20 token smart contracts

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,18 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-  uint256 public totalSupply;
-
-   constructor () public {
-        totalSupply = 1000000;
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
+    uint256 public totalSupply;
+   mapping( address=> uint256 ) public balanceOf;
+   constructor (uint256 _initialSupply) public {
+     balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
-    // function DappTokenSupply () public {
-    //     totalSupply = 1000000;
+    // function DappTokenSupply (uint256 _initialSupply) public {
+    //   balanceOf[msg.sender] = _initialSupply;
+    //     totalSupply = _initialSupply;
     // }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,12 +1,31 @@
 var DappToken=artifacts.require("./DappToken.sol");
 
 contract('DappToken',function(accounts){
-it('sets the total supply upon deployment',function(){
-return DappToken.deployed().then(function(instance){
-tokenInstance=instance;
-return tokenInstance.totalSupply();
-}).then(function(totalSupply){
-    assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000');
-});
-});
+    var tokenInstance;
+
+    it('initializes the contract with the correct values',function(){
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, 'DApp Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, 'DAPP', 'has the correct symbol');
+        }).then(function(standard){
+            assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
+        });
+    });
+    it('allocates the total supply upon deployment',function(){
+        return DappToken.deployed().then(function(instance){
+        tokenInstance=instance;
+      tokenInstance = instance;
+    return tokenInstance.totalSupply();
+    }).then(function(totalSupply){
+        assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000');
+        return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance){
+        assert.equal(adminBalance.toNumber(),1000000,'it allocates the initial supply to the admin account')
+    });
+    });
 });
