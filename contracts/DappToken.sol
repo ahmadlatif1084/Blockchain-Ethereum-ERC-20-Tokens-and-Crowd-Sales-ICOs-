3c335464fb2b0a5c549d7ecf8eb11ec6a750fa70
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-12 19:34:33 +0500

    Delegated Token Transfers

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -23,7 +23,6 @@ contract DappToken {
         totalSupply = _initialSupply;
     }
 // Transfer
-
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
 
@@ -38,10 +37,27 @@ contract DappToken {
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
-
         emit Approval(msg.sender, _spender, _value);
-
         return true;
     }
+
+// transfer Form
+    function transferFrom(address _from,address _to,uint256 _value) public returns (bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+        // Require _from has enough tokens
+        // Require allowance is big enough
+        // change the balance
+        // update the allowance
+        // transfer event
+        // return a boolean
+    }
+
 
 }
