73d9ad8d751c3f361715e781b1a582fa9128ac33
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-12 12:45:05 +0500

    Transferring Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,16 +1,30 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-    string public name = "DApp Token";
-    string public symbol = "DAPP";
-    string public standard = "DApp Token v1.0";
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    string  public standard = "DApp Token v1.0";
     uint256 public totalSupply;
+    event Transfer(
+      address indexed _from,
+      address indexed _to,
+      uint256 _value
+    );
    mapping( address=> uint256 ) public balanceOf;
    constructor (uint256 _initialSupply) public {
      balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-
+// Transfer
+     function transfer(address _to,uint256 _value) public returns (bool success) {
+       require(balanceOf[msg.sender] >= _value);
+       // Transfer the balance
+       balanceOf[msg.sender] -= _value;
+       balanceOf[_to] += _value;
+        // Transfer event
+       emit Transfer(msg.sender, _to, _value);
+       return true;
+      }
     // function DappTokenSupply (uint256 _initialSupply) public {
     //   balanceOf[msg.sender] = _initialSupply;
     //     totalSupply = _initialSupply;
