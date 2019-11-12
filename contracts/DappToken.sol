a02f2f7f777b5ba7e5591915c7e8cb4ebf0ed223
ahmadlatif1084 <ahmad.latif1084@gmail.com>
2019-11-12 21:49:59 +0500

    pulling

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,12 +2,14 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
   uint256 public totalSupply;
-
-   constructor () public {
-        totalSupply = 1000000;
+    mapping (address => uint256) public balanceOf;
+   constructor (uint256 _initialSupply) public {
+       balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
-    // function DappTokenSupply () public {
-    //     totalSupply = 1000000;
+
+    // function DappTokenSupply (uint256 _initialSupply) public {
+    //     totalSupply = _initialSupply;
     // }
 }
