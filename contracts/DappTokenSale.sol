74a2950bbeca666fb71e895fd9372c9f43ecf301
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-15 15:20:15 +0500

    updating

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -4,7 +4,7 @@ contract DappTokenSale {
      address admin;
      DappToken public tokenContract;
      uint256 public tokenPrice;
-     uint256 public tokenSold;
+     uint256 public tokensSold;
      event Sell(address _buyer,uint256 _amount);
     constructor (DappToken _tokenContract,uint256 _tokenPrice) public {
         admin = msg.sender;
@@ -19,17 +19,13 @@ contract DappTokenSale {
     function multiply(uint x,uint y) internal pure returns (uint z){
         require(y == 0 || (z = x * y)/ y == x);
     }
-    function buyTokens(uint256 _numberOfTokens) public payable{
+    function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(this) >= _numberOfTokens);
-        require(tokenContract.transfer(msg.sender,_numberOfTokens));
-        tokenSold += _numberOfTokens;
-        // Require that value is equal to tokens
-        // Require that the contract has enough tokens
-        // Require that a transfer is successfull
-        // keep track of tokenSold
-        // Trigger Sell Event
-        emit Sell(msg.sender,_numberOfTokens);
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -2,30 +2,29 @@ var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappTokenSale',function(accounts){
-    var tokenSaleInstance;
     var tokenInstance;
+    var tokenSaleInstance;
     var admin = accounts[0];
     var buyer = accounts[1];
-    var tokenPrice=1000000000000000; // in wei
+    var tokenPrice = 1000000000000000; // in wei
     var tokensAvailable = 750000;
     var numberOfTokens;
 
-    it('initializes the contract with the correct values',function(){
-        return DappTokenSale.deployed().then(function(instance){
-            tokenSaleInstance=instance;
-            return tokenSaleInstance.address
-        }).then(function(address){
-            assert.notEqual(address,0x0,'has contract address');
-            return tokenSaleInstance.tokenContract();
-        }).then(function(address){
-            assert.notEqual(address,0x0,'has token contract address');
-            return tokenSaleInstance.tokenPrice();
-        }).then(function(price){
-            assert.equal(price,tokenPrice,'token price is correct');
+    it('initializes the contract with the correct values', function() {
+        return DappTokenSale.deployed().then(function(instance) {
+          tokenSaleInstance = instance;
+          return tokenSaleInstance.address
+        }).then(function(address) {
+          assert.notEqual(address, 0x0, 'has contract address');
+          return tokenSaleInstance.tokenContract();
+        }).then(function(address) {
+          assert.notEqual(address, 0x0, 'has token contract address');
+          return tokenSaleInstance.tokenPrice();
+        }).then(function(price) {
+          assert.equal(price, tokenPrice, 'token price is correct');
         });
-    });
-
-    it('facilities token buying',function(){
+      });
+      it('facilities token buying',function(){
         return DappToken.deployed().then(function(instance){
             // Grab token instance first
         tokenInstance = instance;
