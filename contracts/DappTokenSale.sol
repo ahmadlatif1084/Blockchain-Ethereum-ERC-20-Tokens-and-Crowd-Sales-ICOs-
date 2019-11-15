d18e96a4b1a3f20465bf8b319a782d51334eda41
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-15 12:16:19 +0500

    Purchasing ERC-20 Tokens

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -5,6 +5,7 @@ contract DappTokenSale {
      DappToken public tokenContract;
      uint256 public tokenPrice;
      uint256 public tokenSold;
+     event Sell(address _buyer,uint256 _amount);
     constructor (DappToken _tokenContract,uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
@@ -14,14 +15,21 @@ contract DappTokenSale {
         // Token Price
 
     }
+    // multiply
+    function multiply(uint x,uint y) internal pure returns (uint z){
+        require(y == 0 || (z = x * y)/ y == x);
+    }
     function buyTokens(uint256 _numberOfTokens) public payable{
-        tokenSold -= _numberOfTokens;
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender,_numberOfTokens));
+        tokenSold += _numberOfTokens;
         // Require that value is equal to tokens
         // Require that the contract has enough tokens
         // Require that a transfer is successfull
         // keep track of tokenSold
         // Trigger Sell Event
-
+        emit Sell(msg.sender,_numberOfTokens);
 
     }
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -1,9 +1,13 @@
 var DappTokenSale = artifacts.require("./DappTokenSale.sol");
+var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappTokenSale',function(accounts){
     var tokenSaleInstance;
+    var tokenInstance;
+    var admin = accounts[0];
     var buyer = accounts[1];
     var tokenPrice=1000000000000000; // in wei
+    var tokensAvailable = 750000;
     var numberOfTokens;
 
     it('initializes the contract with the correct values',function(){
@@ -22,14 +26,38 @@ contract('DappTokenSale',function(accounts){
     });
 
     it('facilities token buying',function(){
-        return DappTokenSale.deployed().then(function(instance){
+        return DappToken.deployed().then(function(instance){
+            // Grab token instance first
+        tokenInstance = instance;
+        return DappTokenSale.deployed();
+        }).then(function(instance){
+            // then grab token sale instance
         tokenSaleInstance = instance;
-        var numberOfTokens = 10;
+        // Provision 75% of all tokens to the token sale
+        return tokenInstance.transfer(tokenSaleInstance.address, tokenAvailable, {from : admin});
+        }).then(function(receipt){
+        numberOfTokens = 10;
         return tokenSaleInstance.buyTokens(numberOfTokens,{ from : buyer , value : numberOfTokens * tokenPrice})
         }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account the tokens are transferred from tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the account the tokens are transferred to');
             return tokenSaleInstance.tokenSold();
         }).then(function(amount){
             assert.equal(amount.toNumber(),numberOfTokens,'increments the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(),numberOfTokens);
+            return tokenSaleInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(),tokensAvailable - numberOfTokens);
+            return tokenSaleInstance.buyTokens(numberOfTokens,{from : buyer, value : 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >=0,'msg value must equal number of token in wei');
+            return tokenSaleInstance.buyTokens(8000,{from : buyer,value : numberOfTokens * tokenPrice});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >=0,'cannot purchase more tokens than available');
         });  
     });
 
