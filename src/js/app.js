bdcaebdc99495486904fa730480b4c31c4c8b80b
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-21 20:06:56 +0500

    Building out the Crowd Sale Website

---
+++ src/js/app.js
@@ -0,0 +1,55 @@
+App = {
+  web3Provider:null,
+  contracts:{},
+  account:'0x0',
+  init: function(){
+    console.log("App initialized");
+    return App.initWeb3();
+  },
+  initWeb3:function(){
+    if(typeof web3 !== 'undefined'){
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    }else{
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+  initContracts: function(){
+    $.getJSON("DappTokenSale.json", function(dappTokenSale){
+      App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
+      App.contracts.DappTokenSale.setProvider(App.web3Provider);
+      App.contracts.DappTokenSale.deployed().then(function(dappTokenSale){
+        console.log("Dapp Token Sale", dappTokenSale.address);
+      });
+    }).done(function(){
+        $.getJSON("DappToken.json",function(dappTokenSale){
+        App.contracts.DappToken = TruffleContract(dappToken);
+        App.contracts.DappToken.setProvider(App.web3Provider);
+        App.contracts.DappToken.deployed().then(function(dappToken){
+          console.log("Dapp Token Address:", dappToken.address);
+        });
+        return App.render();
+      })
+    })
+  },
+
+  render: function(){
+    web3.eth.getCoinbase(function(err,account){
+      if(err == null){
+        console.log("account",account);
+        App.account = account;
+        $("#accountAddress").html("Your Account:" + account);
+      }
+    })
+  }
+}
+
+$(function(){
+
+  $(window).on('load', function(){
+    App.init();
+  });
+
+});
