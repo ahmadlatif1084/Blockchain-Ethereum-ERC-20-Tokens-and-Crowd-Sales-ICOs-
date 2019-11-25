157f7b783af325da72272ca5229380e2017f0e6f
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-25 19:01:46 +0500

    deploy contracts commands

--- truffle.js
+++ truffle.js
@@ -4,6 +4,12 @@ module.exports={
             host:"127.0.0.1",
             port:"7545",
             network_id:"*"
+        },
+        rinkeby:{
+            host:"localhost",
+            port:8545,
+            network_id:4,
+            gas:470000
         }
     }
 };
